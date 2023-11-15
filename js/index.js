a3e24a9a70d07006edb595ae7b5f9db2175b0443
Julia Schwerdtfeger <js.schwerdt@gmail.com>
2023-11-15 10:39:54 +0100

    challenge 1

--- js/index.js
+++ js/index.js
@@ -11,6 +11,12 @@ console.clear();
 
 // --v-- write your code here --v--
 
+const title = "Neue Fische Bootcamp";
+const text = "I'm currently in week 2";
+const likes = 16;
+const userCreate = "Julia Schwerdtfeger";
+const isReported = true;
+
 // --^-- write your code here --^--
 
 /*
@@ -18,5 +24,11 @@ console.clear();
 */
 
 // --v-- write your code here --v--
+
+console.log(title);
+console.log(text);
+console.log(likes);
+console.log(userCreate);
+console.log(isReported);
 
 // --^-- write your code here --^--
